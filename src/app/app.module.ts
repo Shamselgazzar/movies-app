e20a9010cf6aef204d8492feae2439041973b861
Shamselgazzar <142535823+Shamselgazzar@users.noreply.github.com>
2023-08-31 16:09:15 +0300

    --

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogueComponent
+    CatalogueComponent,
+    HeaderComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
