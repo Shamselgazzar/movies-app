e5b5a857a347784f1271e0239d449a8c5c0e1a82
Shamseldin Elgazzar <selgazzar@sumerge.com>
2023-09-06 16:23:59 +0300

    authService Created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as usersData from './users.json';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  username: string = '';
+export class LoginComponent implements OnInit{
+  email: string = '';
   password: string = '';
+  //users: [] = usersData a;
 
-  onSubmit(){
-    
+  ngOnInit(): void {
+    const users = usersData as any[];
+    console.log(users[0]);
   }
+  
+  authenticate(email: string, password: string): boolean {
+    
+    const users = usersData as any[];
 
+    const user = users.find((u) => u.email === email && u.password === password);
+    return !!user;
+  }
+  
+  onSubmit(){
+    //console.log(usersData);
+  }
+  
 }
