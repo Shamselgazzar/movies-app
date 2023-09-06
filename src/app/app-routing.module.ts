8a7a716a4234e9f8aee07e1535e328ebb4861156
Shamseldin Elgazzar <selgazzar@sumerge.com>
2023-09-06 13:16:40 +0300

    styling - UI enhanced

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: CatalogueComponent },
   { path: 'about', component: AboutComponent },
   { path: 'movie-details/:id', component: MovieDetailsComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full'},
-  //{ path: 'login', component: LoginComponent }
+  
   
   ];
 
