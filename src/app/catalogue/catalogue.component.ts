a1f8656a88bd757a45393d870560355ee831e3f5
Shamseldin Elgazzar <selgazzar@sumerge.com>
2023-09-11 16:42:12 +0300

    cleaning

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 import { TmdbService } from './tmdb.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatPaginator } from '@angular/material/paginator';
-import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -15,33 +14,27 @@ import { Observable } from 'rxjs';
 
 export class CatalogueComponent implements OnInit {
   titleee = 'catalog';
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
  
   isLoading = true;
-  
   movies : any[] = [];
-  movies_ !: Observable<any>;
   filter = 'topRated';
   
-  constructor (private tmdbService : TmdbService,
-      private location: Location,
-      private route: ActivatedRoute) {}
+  constructor (
+    private tmdbService : TmdbService,
+    private location: Location,
+    private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     
     this.route.queryParams.subscribe(params => {
-      
       const receivedData = params['category'];
-      console.log('category', receivedData);
+      console.log('category: ', receivedData);
       this.filter = receivedData;
       this.fetchMovies();
       
     });
     
     localStorage.setItem('currentUrl', this.location.path());
-    
-
   }
 
   fetchMovies(){
@@ -50,14 +43,9 @@ export class CatalogueComponent implements OnInit {
         console.log(data.results.length)
         this.movies = data.results;
         console.log(data.results)
-        
         this.isLoading = false;
       } 
-    );
-
-    // this.movies_ = this.tmdbService.getMovies(1,this.filter)
-    // this.isLoading = false;
-    
+    );  
   }
 
   loadMoreMovies(){
@@ -72,7 +60,6 @@ export class CatalogueComponent implements OnInit {
       } 
     );
   }
-
 
   
   goUp(){
