b02a972d993f6b4e0c2ed63e329de466f3fcb4b7
Shamseldin Elgazzar <selgazzar@sumerge.com>
2023-09-12 16:26:47 +0300

    tmdb service enhanced

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import {environment} from '../../environments/environment';
 
@@ -9,15 +9,15 @@ import {environment} from '../../environments/environment';
 })
 
 export class TmdbService {
-  filter = new BehaviorSubject<string>('topRated');
+
 
   private pageNumber  = 1;
   public counter = 1;
   private apiKey = environment.apiKey;
-  // private apiUrl = 'https://api.themoviedb.org/3';
-  // private url = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
+
 
   constructor(private http: HttpClient) { }
+
 
   getMovieDetails(movieId: string): Observable<any> {
     const url0 = 'https://api.themoviedb.org/3/movie/'+(movieId)+'?api_key='+this.apiKey;
@@ -44,24 +44,6 @@ export class TmdbService {
     }
   
     return this.http.get(apiUrl);
-  }
-  
-
-  getPopularMovies(pageNumber:number = this.pageNumber): Observable<any> {
-    const popularUrl = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page='+pageNumber+'&api_key='+this.apiKey;
-    console.log(popularUrl)
-    return this.http.get(popularUrl);
-  }
-
-  discoverMovies(pageNumber:number = this.pageNumber):Observable<any>{
-    const discoverUrl = 'https://api.themoviedb.org/3/discover/movie?language=en-US&page='+pageNumber+'&api_key='+this.apiKey;
-    return this.http.get(discoverUrl);
-  }
-
-  getTopRatedMovies(pageNumber:number = this.pageNumber): Observable<any>{
-    const topRatedUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page='+pageNumber+'&api_key='+this.apiKey;
-    return this.http.get(topRatedUrl);
-  
   }
 
 
