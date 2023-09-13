630790a57477be9ba9ad4a4f178de16dfbfc10aa
Shamseldin Elgazzar <selgazzar@sumerge.com>
2023-09-13 13:36:53 +0300

    page not found added with routing

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
-  
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthenticationRoutingModule { }
+export class SharedRoutingModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
+import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './header/header.component';
-
-
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
+    SharedRoutingModule,
     RouterModule
   ],
   exports: [
     HeaderComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
