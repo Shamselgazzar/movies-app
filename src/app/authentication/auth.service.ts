527d2837498147be5c89968e57d94e8bba17a7b8
Shamseldin Elgazzar <selgazzar@sumerge.com>
2023-09-14 18:50:33 +0300

    ++

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, lastValueFrom } from 'rxjs';
+import { BehaviorSubject, lastValueFrom } from 'rxjs';
 
 
 
